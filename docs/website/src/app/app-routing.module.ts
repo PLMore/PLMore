e211be6781c18de63f00723bd3c755eb60d46bfa
Mellywins <zouaghi.wow1919@gmail.com>
2022-06-10 12:32:33 +0100

    website work

--- docs/website/src/app/app-routing.module.ts
+++ docs/website/src/app/app-routing.module.ts
@@ -1,7 +1,13 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { MainComponent } from './home/main/main.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {
+    path: "",
+    component: MainComponent
+  }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- docs/website/src/app/app.module.ts
+++ docs/website/src/app/app.module.ts
@@ -3,14 +3,22 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { HeaderComponent } from './shared/header/header.component';
+import { FooterComponent } from './shared/footer/footer.component';
+import { MainComponent } from './home/main/main.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HeaderComponent,
+    FooterComponent,
+    MainComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    BrowserAnimationsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
