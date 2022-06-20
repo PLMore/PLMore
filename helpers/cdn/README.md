89b034a1a0279078de1a42a6c61357a4ccd2f0bd
Mellywins <zouaghi.wow1919@gmail.com>
2022-06-20 15:31:43 +0100

    moved the CDN project to the organization specific repo

--- helpers/cdn/README.md
+++
@@ -1,6 +0,0 @@
-# PLMore Content Delivery Network.
-
-This part will hold the backend application that streams the videos chunk by chunk to the frontend apps to enhance the content load time.
-
-This app is deployed by a container on heroku:
-https://plmore-cdn.herokuapp.com
--- helpers/cdn/src/videos/videos.module.ts
+++
@@ -1,9 +0,0 @@
-import { Module } from '@nestjs/common';
-import { VideosService } from './videos.service';
-import { VideosController } from './videos.controller';
-
-@Module({
-  controllers: [VideosController],
-  providers: [VideosService]
-})
-export class VideosModule {}
